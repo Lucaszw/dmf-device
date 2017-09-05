45980d4e553777fbbaf7b688dadba39720a15a8b
lucaszw <lucas.zeer@gmail.com>
2017-09-05 13:25:15 -0400

    remove paho_mqtt_helper dependency

--- setup.py
+++ setup.py
@@ -5,12 +5,12 @@ from setuptools import setup
 sys.path.insert(0, '.')
 import version
 
-
 setup(name='dmf-device',
       version=version.getVersion(),
       description=open('README.md', 'rb').read(),
       author='Christian Fobel',
       author_email='christian@fobel.net',
       url='https://github.com/Lucaszw/dmf_device',
-      install_requires=['paho-mqtt', 'wheezy'],
-      packages=['paho_mqtt_helpers'])
+      install_requires=['droplet-planning', 'lxml', 'networkx', 'numpy',
+                        'pandas', 'path_helpers','pyyaml', 'svg-model'],
+      packages=['dmf_device'])
